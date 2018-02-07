4474c1079fd0229abc7942456cd2a3434f2c8efb
Plinio Vargas <plinio.vargas@verizon.net>
2018-02-07 08:04:04 -0500

    update

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -2,6 +2,11 @@ var Selection = d3.select("#main-vis");
 var visWidth = parseInt(Selection.style("width"));
 var visHeight = parseInt(Selection.style("height"));
 
+// add touch to mouseover and mouseout
+var over = "ontouchstart" in window ? "touchstart" : "mouseover";
+var out = "ontouchstart" in window ? "touchend" : "mouseout";
+
+
 var margin = {top: 20, right: 20, bottom: 100, left: 40},
     width = visWidth - margin.left - margin.right,
     height = visHeight - margin.top - margin.bottom - 15;
@@ -15,20 +20,31 @@ var margin = {top: 20, right: 20, bottom: 100, left: 40},
 
 // setup x
 var xValue = function(d) {
-			return +d.timestamp;
+			return d.x;
     },
     xScale = d3.scale.linear().range([0, width]), // value -> display
     xMap = function(d) { return xScale(xValue(d));}, // data -> display
     xAxis = d3.svg.axis().scale(xScale).orient("bottom");
 
+/*
 // setup y
 var yValue = function(d) {
     //return d.ave_injury;
-    return d.idx;
+    return d.y;
     }, // data -> value
     yScale = d3.scale.linear().range([height, 0]), // value -> display
     yMap = function(d) { return yScale(yValue(d));}, // data -> display
     yAxis = d3.svg.axis().scale(yScale).orient("left");
+*/
+// setup y
+var yValue = function(d) {
+    //return d.ave_injury;
+    return d.archived_at;
+    }, // data -> value
+    yScale = d3.scale.ordinal().rangeRoundBands([height, 0]),
+    yMap = function(d) { return yScale(yValue(d));}, // data -> display
+    yAxis = d3.svg.axis().scale(yScale).orient("left");
+
 
 // setup fill color
 var cValue = function(d) { return " ";},
@@ -46,7 +62,7 @@ var tooltip = Selection.append("div")
     .attr("class", "tooltip")
     .style("opacity", 0);
 
-var resource_data = [];
+
 // Get the data
 d3.json("data/recreate.json", function(error, data) {
     var today = new Date();
@@ -54,7 +70,7 @@ d3.json("data/recreate.json", function(error, data) {
 
     // unpack root-memento features
     var root_obj = {};
-    root_obj['timestamp'] = +data[0].root_memento.timestamp;
+    root_obj['timestamp'] = convertToDate(data[0].root_memento.timestamp);
     root_obj['url'] = data[0].root_memento.url;
     root_obj['archieved_at'] = data[0].root_memento.archived_at;
 
@@ -79,27 +95,31 @@ d3.json("data/recreate.json", function(error, data) {
         }
 
         val.y = y_value;
-        val.x = d.timestamp - root_obj.timestamp;
+        val.x = new Date(convertToDate(d.timestamp).getTime() - root_obj.timestamp.getTime()).getTime();
+        val.archieved_at = d.archived_at;
+
+        val.freq = 1;
 
         resource_plot_values .push(val);
     });
 
-    console.log(resource_plot_values);
 
-    plotResourceGraph(data);
+    plotResourceGraph(resource_plot_values);
 });
 
 // load data
 function plotResourceGraph(data) {
 	// don't want dots overlapping axis, so add in buffer to data domain
 	xScale.domain([d3.min(data, xValue)-1, d3.max(data, xValue)+1]);
-	yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+	//yScale.domain([d3.min(data, yValue)-1, d3.max(data, yValue)+1]);
+
+	yScale.domain(data.map(function(d) { return d.archived_at; }));
 
 	var xPosition;
 	var yPosition;
 
     var title = 'Average Injuries by Age';
-    var yText = 'Average Number of Injuries';
+    var yText = '';
     var radius = 3.5;
     var minRadious = 2.7;
 
@@ -107,7 +127,6 @@ function plotResourceGraph(data) {
 	svg.append("g")
 	  .attr("class", "x axis")
 	  .attr("transform", "translate(0," + height + ")")
-	  .style("font-size", xAxisFontSize)
 	  .call(xAxis)
 	.append("text")
 		.attr("class", "label")
@@ -119,7 +138,6 @@ function plotResourceGraph(data) {
 	// y-axis
 	svg.append("g")
 	  .attr("class", "y axis")
-	  .style("font-size", yAxisFontSize)
 	  .call(yAxis)
 	.append("text")
 	  .attr("class", "label")
@@ -135,7 +153,7 @@ function plotResourceGraph(data) {
 	.enter().append("circle")
 	  .attr("class", "dot")
 	  .attr("r", function (d) {
-		  return  d.count / (2 * Math.log(d.count * 10)) * radius + minRadious;
+		  return  d.freq / (2 * Math.log(d.freq * 10)) * radius + minRadious;
       })
 	  .attr("cx", xMap)
 	  .attr("cy", yMap)
@@ -149,7 +167,6 @@ function plotResourceGraph(data) {
 		.attr("x", (width / 2))
 		.attr("y", 0 - (margin.top / 3) + 20)
 		.attr("text-anchor", "middle")
-		.style("font-size", titleFontSize)
 		.text(title);
 
     // show tooltip of vertices
@@ -157,13 +174,13 @@ function plotResourceGraph(data) {
     	xPosition = d3.event.pageX + 10;
     	yPosition = d3.event.pageY;
 
-    	if (xPosition >= pageWidth - 100 )
+    	if (xPosition >= visWidth - 100 )
     		xPosition -= 100;
 
         tooltip.style("opacity", 0.9)
-            .html("<strong>timestamp</strong>: " + d.timestamp + "<br />" +
-                  "<strong>Ave Injuries</strong>: " + (d.aveInjuries).toFixed(2) +
-			      "<br /> <strong>Personnel Inj</strong>:"  +  d.count + "<br/>")
+            .html("<strong>timestamp</strong>: " + d.x + "<br />" +
+                  "<strong>Time</strong>: " + (d.x).toFixed(2) +
+			      "<br /> <strong>Personnel Inj</strong>:"  +  d.freq + "<br/>")
             .style("left", xPosition + "px")
 			.style("top", yPosition + "px");
     }
@@ -172,4 +189,14 @@ function plotResourceGraph(data) {
     function verticesTooltipHide() {
       tooltip.style("opacity", 0);
     }
+}
+
+function convertToDate(obj) {
+    return new Date(parseInt(obj.substr(0,4)),
+                    parseInt(obj.substr(4,2)) - 1,
+                    parseInt(obj.substr(6,2)),
+                    parseInt(obj.substr(8,2)),
+                    parseInt(obj.substr(10,2)),
+                    parseInt(obj.substr(12,2))
+    );
 }
